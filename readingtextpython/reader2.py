751287146ac168d9cf12afa5ab60c34410f8f730
MatijaaBabic <BabicM@dulwich.org.uk>
2022-12-05 13:15:07 +0000

    d

---
+++ readingtextpython/reader2.py
@@ -0,0 +1,5 @@
+import os
+os.chdir(r'C:\Users\Windows 10\Desktop\Microsoft\Prezentacije\Programs for school\Code\Github\MatijaB-classwork\readingtextpython')
+f = open('nd.txt', 'r')
+data = f.read(1)
+print(data)
