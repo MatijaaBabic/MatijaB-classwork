c3c4e473708a27a477d5981fa67d63733d1e31dd
MatijaaBabic <BabicM@dulwich.org.uk>
2023-12-13 18:23:50 +0000

    dfs

--- DataStructuresorADT/graph.py
+++ DataStructuresorADT/graph.py
@@ -1,6 +1,3 @@
-import networkx as nx
-import matplotlib.pyplot as plt
-
 
 # Example graph represented as an adjacency list 
 graph = { 'A': ['B', 'C'], 'B': ['A', 'D'], 'C': ['A', 'E', 'F'], 'D': ['B', 'G'], 'E': ['C', 'H'], 'F': ['C', 'I'], 'G': ['D', 'J', 'K'], 'H': ['E', 'K', 'L'], 'I': ['F', 'L', 'J'], 'J': ['G', 'I'], 'K': ['G', 'H'], 'L': ['H', 'I'] }
@@ -11,18 +8,33 @@ def generate_edges(g):
     for node in g: 
         for neighbour in g[node]: 
             edges.append((node, neighbour)) 
-    return edges 
+        #next neighbour
+    #next node
+    return edges
+#end function 
 
 print(generate_edges(graph))
+#finding shortest paths
+def bfs_shortest_paths(g, start):
+    paths = {node: [] for node in g}
+    paths[start] = [start]
 
-g = nx.Graph(graph)
- 
-g.add_edge(1, 2)
-g.add_edge(2, 3)
-g.add_edge(3, 4)
-g.add_edge(1, 4)
-g.add_edge(1, 5)
- 
-nx.draw(g)
-plt.savefig("filename.png")
+    queue = [start]
 
+    while queue:
+        current_node = queue.pop(0)
+
+        for neighbour in g[current_node]:
+            if not paths[neighbour]:
+                paths[neighbour] = paths[current_node] + [neighbour]
+                queue.append(neighbour)
+            #endif
+        #next neighbour
+    #endwhile
+    
+
+    return paths
+#end function
+
+print("Shortest paths from 'A' to other nodes are:", bfs_shortest_paths(graph, 'A'))
+print("Shortest paths from 'D' to other nodes are:", bfs_shortest_paths(graph, 'D'))
